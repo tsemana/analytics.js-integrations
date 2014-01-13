5ef6fbf75f245284aeae9c73371fe31c1308370e
Amir Abu Shareb <yields@icloud.com>
2014-01-13 13:36:48 +0200

    add facebook

---
+++ test/integrations/facebook.js
@@ -0,0 +1,72 @@
+
+describe('Facebook', function(){
+
+  var Facebook = require('integrations/lib/facebook');
+  var test = require('integration-tester');
+  var analytics = require('analytics');
+  var assert = require('assert');
+  var sinon = require('sinon');
+  var facebook;
+
+  var settings = {
+    events: {
+      signup: 0,
+      login: 1,
+      play: 2
+    }
+  };
+
+  beforeEach(function(){
+    analytics.use(Facebook);
+    facebook = new Facebook.Integration(settings);
+    facebook.initialize();
+  })
+
+  it('should have the correct settings', function(){
+    test(facebook)
+      .name('Facebook')
+      .readyOnInitialize()
+      .option('currency', 'USD')
+      .option('events', {});
+  })
+
+  describe('#track', function(){
+    before(function(){
+      sinon.spy(Facebook, 'load');
+    })
+
+    afterEach(function(){
+      Facebook.load.reset();
+    })
+
+    it('should not send if event is not define', function(){
+      test(facebook).track('toString', {});
+      assert(!Facebook.load.called);
+    })
+
+    it('should send event if found', function(){
+      test(facebook)
+        .track('signup', {})
+        .called(Facebook.load)
+        .with(0, 0, 'USD');
+    })
+
+    it('should send revenue', function(){
+      test(facebook)
+        .track('login', { revenue: '$50' })
+        .called(Facebook.load)
+        .with(1, 50, 'USD');
+    })
+
+    it('should send correctly', function(){
+      test(facebook).track('play', { revenue: 90 });
+      var img = Facebook.load.returnValues[0];
+      assert(img);
+      assert(img.src == 'http://www.facebook.com/offsite_event.php'
+        + '?currency=USD'
+        + '&value=90'
+        + '&id=2');
+    })
+  })
+
+})
