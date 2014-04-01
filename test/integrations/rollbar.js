7c7fb1643bff4feae5339cf7de41123ffe74aab0
Cory Virok <coryvirok@gmail.com>
2014-03-31 18:52:21 -0700

    fix rollbar library to create shim in initialize() and do the full load via load()

--- test/integrations/rollbar.js
+++ test/integrations/rollbar.js
@@ -1,27 +1,30 @@
-
 describe('Rollbar', function() {
 
   var analytics = require('analytics');
   var assert = require('assert');
-  var equal = require('equals');
-  var noop = function(){};
   var Rollbar = require('integrations/lib/rollbar');
   var sinon = require('sinon');
   var test = require('integration-tester');
 
   var rollbar;
   var settings = {
-    accessToken: 'e1674422cbe9419987eb2e7f98adc5ec',
+    accessToken: 'FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF',
     environment: 'testenvironment',
   };
 
+  var prevOnError;
+
   beforeEach(function() {
+    prevOnError = window.onerror;
     analytics.use(Rollbar);
     rollbar = new Rollbar.Integration(settings);
+    //rollbar.initialize();
   });
 
   afterEach(function() {
     rollbar.reset();
+    analytics.user().reset();
+    window.onerror = prevOnError;
   });
 
   it('should have the right settings', function() {
@@ -34,49 +37,74 @@ describe('Rollbar', function() {
   });
 
   describe('#initialize', function() {
-    beforeEach(function () {
+    beforeEach(function() {
+      prevOnError = window.onerror;
       sinon.stub(rollbar, 'load');
+      rollbar.reset();
     });
 
-    it('should call #load', function () {
+    afterEach(function() {
+      window.onerror = prevOnError;
+    });
+
+    it('should create the window.Rollbar object', function() {
+      assert(!window.Rollbar);
+      rollbar.initialize();
+      assert(window.Rollbar);
+    });
+
+    it('should have all of the correct methods', function() {
+      rollbar.initialize();
+      assert(window.Rollbar);
+      assert(window.Rollbar.debug);
+      assert(window.Rollbar.info);
+      assert(window.Rollbar.warning);
+      assert(window.Rollbar.error);
+      assert(window.Rollbar.critical);
+      assert(window.Rollbar.configure);
+      assert(window.Rollbar.scope);
+    });
+
+    it('should set window.onerror', function() {
+      var onerr = window.onerror;
+      rollbar.initialize();
+      assert(window.onerror !== onerr);
+      assert(typeof window.onerror === 'function');
+    });
+
+    it('should call #load', function() {
       rollbar.initialize();
       assert(rollbar.load.called);
     });
   });
 
   describe('#loaded', function() {
-    it('should be loaded after initialize', function() {
+    it('should test window.Rollbar and .shimId for loaded', function() {
+      window.Rollbar = null;
       assert(!rollbar.loaded());
-      rollbar.initialize();
+      window.Rollbar = {};
+      window.Rollbar.shimId = 1;
+      assert(!rollbar.loaded());
+      delete window.Rollbar.shimId;
       assert(rollbar.loaded());
     });
   });
 
   describe('#load', function() {
     beforeEach(function() {
-      rollbar.reset();
+      sinon.stub(rollbar, 'load');
+      rollbar.initialize();
+      rollbar.load.restore();
     });
 
-    it('should set an onerror handler', function (done) {
-      var handler = window.onerror;
+    it('should change loaded state', function(done) {
+      var shim = window.Rollbar;
+      assert(!rollbar.loaded());
       rollbar.load(function(err) {
         if (err) return done(err);
-        assert(handler !== window.onerror);
-        assert('function' === typeof window.onerror);
-        done();
-      });
-    });
-
-    it('should add global Rollbar notifier', function(done) {
-      rollbar.load(function(err) {
-        assert(window.Rollbar);
-        assert(window.Rollbar.debug);
-        assert(window.Rollbar.info);
-        assert(window.Rollbar.warning);
-        assert(window.Rollbar.error);
-        assert(window.Rollbar.critical);
-        assert(window.Rollbar.configure);
-        assert(window.Rollbar.scope);
+        assert(rollbar.loaded());
+        assert(window.Rollbar !== shim);
+        assert(typeof window.Rollbar.error === 'function');
         done();
       });
     });
@@ -87,36 +115,64 @@ describe('Rollbar', function() {
     beforeEach(function() {
       rollbar.initialize();
       configure = sinon.stub(window.Rollbar, 'configure');
+      rollbar.initialize();
     });
 
-    it('should add an id to metadata', function() {
-      test(rollbar).identify('user-id');
-      var firstCall = window.Rollbar.configure.firstCall;
-      args = firstCall.args;
-      assert(args[0].payload.person.id === 'user-id');
+    afterEach(function() {
+      rollbar.reset();
+      configure.restore();
     });
 
-    it('should add traits to person data', function() {
-      test(rollbar).identify(null, {trait: true});
-      var firstCall = window.Rollbar.configure.firstCall;
-      args = firstCall.args;
-      assert(args[0].payload.person.id === null);
-      assert(args[0].payload.person.trait === true);
+    it('should send an id', function() {
+      test(rollbar).identify('id', {});
+      assert(configure.calledWith({payload: {person: {id: 'id'}}}));
     });
 
-    it('should add an id and traits to person data', function() {
-      test(rollbar).identify('user-id', {trait: true});
-      var firstCall = window.Rollbar.configure.firstCall;
-      args = firstCall.args;
-      assert(args[0].payload.person.id === 'user-id');
-      assert(args[0].payload.person.trait === true);
+    it('should not send only traits', function() {
+      test(rollbar).identify(null, { trait: true });
+      assert(!configure.called);
     });
 
-    it('should not add to person data when identify option is false', function() {
-      rollbar.options.identify = false;
-      test(rollbar).identify('user-id');
-      assert(!window.Rollbar.configure.called);
+    it('should send an id and traits', function() {
+      test(rollbar).identify('id', { trait: true });
+      assert(configure.calledWith({payload: {person: {id: 'id', trait: true }}}));
+    });
+
+  });
+
+  describe('window.onerror', function() {
+    var prev;
+    beforeEach(function() {
+      // neuter the original window.onerror so we don't call it after the rollbar
+      // onerror handler fires.
+      prev = sinon.stub(window, 'onerror');
+    });
+
+    afterEach(function() {
+      // Put the original window.onerror function back
+      prev.restore();
+    });
+
+    it("should call window.Rollbar.uncaughtError", function(done) {
+      rollbar.initialize();
+      rollbar.load(function() {
+        var onerr = window.onerror;
+        var spy = sinon.spy(window.Rollbar, 'uncaughtError');
+        onerr('test message', 'http://foo.com', 33, 21, new Error('testing'));
+
+        assert(spy.called);
+
+        var call = spy.getCall(0);
+        var args = call.args;
+
+        assert(args.length == 5);
+        assert(args[0] == 'test message');
+        assert(args[1] == 'http://foo.com');
+        assert(args[2] == 33);
+        assert(args[3] == 21);
+        done();
+      });
     });
   });
-});
 
+});
