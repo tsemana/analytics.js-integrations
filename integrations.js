df163668799be5dcf7e5f04d1f96e3f3cd58a4f8
jspdown <harold.ozouf@epitech.eu>
2014-06-16 17:40:00 -0700

    Hublo integration

--- integrations.js
+++ integrations.js
@@ -33,6 +33,7 @@ module.exports = [
   require('./lib/heap'),
   require('./lib/hellobar'),
   require('./lib/hittail'),
+  require('./lib/hublo'),
   require('./lib/hubspot'),
   require('./lib/improvely'),
   require('./lib/inspectlet'),
