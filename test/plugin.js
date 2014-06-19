1d82da4f1aacea958fab2f8f41ade82d9053f087
Lance Pollard <lancejpollard@gmail.com>
2014-06-19 16:30:15 -0700

    pair program finish adroll test refactoring

--- test/plugin.js
+++ test/plugin.js
@@ -5,7 +5,6 @@
 
 var indexOf = require('indexof');
 var assert = require('assert');
-var merge = require('merge');
 var each = require('each');
 var keys = require('keys');
 var fmt = require('fmt');
@@ -25,114 +24,111 @@ module.exports = plugin;
 
 function plugin(analytics) {
   analytics.spies = [];
-  merge(analytics, proto);
-};
 
-/**
- * Prototype.
- */
+  /**
+   * Spy on a `method` of host `object`.
+   *
+   * @param {Object} object
+   * @param {String} method
+   * @return {Analytics}
+   */
 
-var proto = {};
-
-/**
- * Spy on a `method` of host `object`.
- *
- * @param {Object} object
- * @param {String} method
- * @return {Analytics}
- */
-
-proto.spy = function(object, method){
-  var s = spy(object, method);
-  this.spies.push(s);
-  return this;
-};
-
-/**
- * Assert that a `spy` was called with `args...`
- *
- * @param {Spy} spy
- * @param {Mixed} args... (optional)
- * @return {Analytics}
- */
-
-proto.called = function(spy){
-  assert(
-    ~indexOf(this.spies, spy), 
-    'You must call `.spy(object, method)` prior to calling `.called()`.'
-  );
-  assert(spy.called, fmt('Expected "%s" to have been called.', spy.name));
-
-  var args = [].slice.call(arguments, 1);
-  if (!args.length) return this;
-
-  assert(
-    spy.got.apply(spy, args), fmt(''
-    + 'Expected "%s" to be called with "%o", '
-    + 'but it was called with "%o".'
-    , spy.name
-    , args
-    , spy.args[0])
-  );
-
-  return this;
-};
-
-/**
- * Call `reset` on the integration.
- *
- * @return {Analytics}
- */
-
-proto.reset = function(){
-  // reset spies
-  each(this.spies, function(s){ s.restore(); });
-  this.spies = [];
-
-  // reset integrations
-  for (var name in this._integrations) {
-    this._integrations[name].reset();
-  }
-
-  // reset stores
-  this.user().reset();
-  // this.group().reset();
-  return this;
-};
-
-/**
- * TODO: add to analytics.js
- *
- * @param {String} name
- */
-
-proto.integration = function(name){
-  return this._integrations[name];
-};
-
-/**
- * Assert a `value` is truthy.
- *
- * @param {Mixed} value
- * @return {Tester}
- */
-
-proto.assert = function(value){
-  assert(value);
-  return this;
-};
-
-/**
- * Expose all of the methods own `assert`.
- *
- * @param {Mixed} args...
- * @return {Tester}
- */
-
-each(keys(assert), function(key){
-  proto[key] = function(){
-    var args = [].slice.call(arguments);
-    assert[key].apply(assert, args);
+  analytics.spy = function(object, method){
+    var s = spy(object, method);
+    this.spies.push(s);
     return this;
   };
-});
+
+  /**
+   * Restore all spies.
+   *
+   * @return {Analytics}
+   */
+
+  analytics.restore = function(){
+    each(this.spies, function(spy, i){
+      spy.restore();
+    });
+    this.spies = [];
+    return this;
+  };
+
+  /**
+   * Assert that a `spy` was called with `args...`
+   *
+   * @param {Spy} spy
+   * @param {Mixed} args... (optional)
+   * @return {Analytics}
+   */
+
+  analytics.called = function(spy){
+    assert(
+      ~indexOf(this.spies, spy), 
+      'You must call `.spy(object, method)` prior to calling `.called()`.'
+    );
+    assert(spy.called, fmt('Expected "%s" to have been called.', spy.name));
+
+    var args = [].slice.call(arguments, 1);
+    if (!args.length) return this;
+
+    assert(
+      spy.got.apply(spy, args), fmt(''
+      + 'Expected "%s" to be called with "%o", '
+      + 'but it was called with "%o".'
+      , spy.name
+      , args
+      , spy.args[0])
+    );
+
+    return this;
+  };
+
+  /**
+   * Call `reset` on the integration.
+   *
+   * @return {Analytics}
+   */
+
+  analytics.reset = function(){
+    this.user().reset();
+    this.group().reset();
+    return this;
+  };
+
+  /**
+   * TODO: add to analytics.js
+   *
+   * @param {String} name
+   */
+
+  analytics.integration = function(name){
+    return this._integrations[name];
+  };
+
+  /**
+   * Assert a `value` is truthy.
+   *
+   * @param {Mixed} value
+   * @return {Tester}
+   */
+
+  analytics.assert = function(value){
+    assert(value);
+    return this;
+  };
+
+  /**
+   * Expose all of the methods own `assert`.
+   *
+   * @param {Mixed} args...
+   * @return {Tester}
+   */
+
+  each(keys(assert), function(key){
+    analytics[key] = function(){
+      var args = [].slice.call(arguments);
+      assert[key].apply(assert, args);
+      return this;
+    };
+  });
+};
