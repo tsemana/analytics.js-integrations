82325b4d9b7bf0295f2a63d6b62accb4385abda1
Suresh <mail@hsuresh.com>
2014-12-10 17:57:38 +0530

    Nudgespot integration

--- test/index.js
+++ test/index.js
@@ -21,7 +21,7 @@ mocha.setup({
 
 describe('integrations', function(){
   it('should export our integrations', function(){
-    assert.equal(75, object.length(Integrations));
+    assert.equal(76, object.length(Integrations));
   });
 });
 
